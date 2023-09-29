e0ce35f5627b6fca301698c49fec9cc8c946dd44
unknown <congnd@solarapp.asia>
2023-09-29 21:04:48 +0700

    update

--- src/components/SliderLanding.tsx
+++ src/components/SliderLanding.tsx
@@ -1,4 +1,5 @@
 import { useState } from "preact/hooks";
+import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from "react-icons/bs";
 
 function SliderLanding(props: any) {
   const [current, setCurrent] = useState(0)
@@ -18,8 +19,22 @@ function SliderLanding(props: any) {
   }
 
   return (
-    <section class='slider'>
-    
+    <section class='slider relative h-screen flex justify-center items-center'>
+      <button>
+        
+      </button>
+      <button>
+
+      </button>
+      {/* <BsFillArrowLeftCircleFill class='left-arrow absolute top-1/2 left-32px text-3xl text-black z-10 cursor-pointer select-none' onclick={prevSlide} />
+      <BsFillArrowRightCircleFill class='right-arrow absolute top-1/2 left-32px text-3xl text-black z-10 cursor-pointer select-none' onclick={nextSlide} /> */}
+      {images.map((slide, index) => {
+        return (
+          <div class={index === current ? 'slide activate' : 'slide'} key={index}>
+            {index === current && (<img src={slide.image} alt='travel image' class='image' />)}
+          </div>  
+        )
+      })}
     </section>
   )
 }
